c113d72d2d59f7b7b116529af6a8e868b6be49a9
Yanpei Guo <2847399255@qq.com>
2023-05-23 13:43:26 +0800

    some modification

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[]) {
     assert(argc > 1);
-    printf("The hash is ", argv[1]);
+    printf("The hash is ");
     Keccak256Result res = hash(argv[1], strlen(argv[1]));
     for (int i = 0; i < BYTES; i++) {
         printf("%02x", res.hash_result[i]);
